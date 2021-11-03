4be6db8b9fc122703c0347dd846864b626c3e1e3
vineetbansal <vineetbansal@protonmail.com>
2021-11-03 19:45:51 +0000

    Deploying to gh-pages from @ PrincetonUniversity/magnet@747e993403ad970a7f8dc1b2d595d30e8405fb9e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gcb5a7c4',
+    VERSION: '0.1.dev1+g747e993',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
