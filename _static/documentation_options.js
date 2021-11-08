970a5eea12b13816881748c13ae298a96a570989
minjiechen <minjie@ieee.org>
2021-11-08 14:49:31 +0000

    Deploying to gh-pages from @ PrincetonUniversity/magnet@773074f8d982ad3c0dafae2a2d4062877cea7193 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gd0f3258',
+    VERSION: '0.1.dev1+g773074f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
